3a101f6f3fd79a80db3a4743fda59d018cca35c4
Vyacheslav Korneev <vchslv13@ukr.net>
2019-06-09 20:11:37 +0300

    Protect several model fields from changing through API

--- social_network/serializers.py
+++ social_network/serializers.py
@@ -6,6 +6,7 @@ class UserSerializer(HyperlinkedModelSerializer):
     class Meta:
         model = User
         fields = ('url', 'id', 'username', 'full_name', 'date_joined', 'email',)
+        read_only_fields = ('date_joined',)
 
 
 class PostSerializer(HyperlinkedModelSerializer):
@@ -13,3 +14,4 @@ class PostSerializer(HyperlinkedModelSerializer):
         model = Post
         fields = ('url', 'id', 'text', 'author', 'creation_datetime',
                   'liked_by_users',)
+        read_only_fields = ('liked_by_users', 'creation_datetime',)
