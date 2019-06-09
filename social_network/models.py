3c008c42c39714a47717f6a4a436eefdfe5f05ff
Vyacheslav Korneev <vchslv13@ukr.net>
2019-06-09 16:36:59 +0300

    Add Post model

--- social_network/models.py
+++ social_network/models.py
@@ -1,6 +1,8 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
 from django.db.models import fields
+from django.db.models.fields.related import ForeignKey, ManyToManyField
+from django.utils import timezone
 
 
 class User(AbstractUser):
@@ -8,3 +10,10 @@ class User(AbstractUser):
     last_name = None
     email = fields.EmailField(unique=True)
     full_name = fields.CharField(max_length=250)
+
+
+class Post(models.Model):
+    creation_datetime = fields.DateTimeField(default=timezone.now)
+    text = fields.TextField(max_length=5000)
+    author = ForeignKey(User, on_delete=models.CASCADE)
+    liked_by_users = ManyToManyField(User, related_name='liked_posts')
